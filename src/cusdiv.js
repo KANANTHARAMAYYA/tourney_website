b3066501fb5b5ac8bfbfc3b27b59266f91b6c1cb
KANANTHARAMAYYA <kanantharamayya@gmail.com>
2021-09-21 06:57:14 -0700

    adding rounded medium fonts

--- src/cusdiv.js
+++ src/cusdiv.js
@@ -2,4 +2,7 @@ import styled from "styled-components";
 
 export const Cusdiv = styled.div`
     height : ${(props)=>props.height};
+    width : ${(props)=>props.width};
+    margin-top : ${(props)=>props.mtop}%;
+    margin-left : ${(props)=>props.mleft}%;
 `;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import Footer from "../components/Footer/Footer.js";
+import { Cusdiv } from "../Cusdiv.js";
 
 function Home(){
     return (
         <div>
+            <Cusdiv height = "100vh" width = "50vh" mtop = "1" mleft ="2">trash</Cusdiv>
+            <Cusdiv height = "500px" width = "500px" mtop = "80" mleft = "30">
             <Footer></Footer>
+            </Cusdiv>
         </div>
     );
 }
