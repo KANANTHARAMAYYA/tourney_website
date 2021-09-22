f9128262ebc15960ae3dcf64337fa63a46c6272d
KANANTHARAMAYYA <kanantharamayya@gmail.com>
2021-09-21 20:10:28 -0700

    cusdiv

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,9 +3,11 @@ import { Cusdiv } from "../../Cusdiv.js";
 import {Nutton} from "../Nutton.js";
 function Login() {
     return (
-    <Cusdiv width="25%" height=" 60vh" mtop="10" mleft="35">
-        Login yo
-        <Nutton></Nutton>
+    <Cusdiv width="25%" height= "60vh" mtop="10" mleft="35">
+        <p Style="font-weight: bold">Login</p>
+        <Nutton>UserName</Nutton>
+        <Nutton>Password</Nutton>
+        <Nutton>Login</Nutton>
     </Cusdiv>
     );
 }
